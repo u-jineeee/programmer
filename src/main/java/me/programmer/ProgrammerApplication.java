7cc09335df535d32467b59c10c9a06f713e2fbcd
ujineeee <yujin11006@naver.com>
2024-11-21 00:49:32 +1000

    initial commit

---
+++ src/main/java/me/programmer/ProgrammerApplication.java
@@ -0,0 +1,13 @@
+package me.programmer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrammerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/programmer/ProgrammerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.programmer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
