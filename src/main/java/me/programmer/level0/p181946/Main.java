8c0a6a2d3cc70ed8449fd7d6199703c818d0d37e
ujineeee <yujin11006@naver.com>
2024-11-26 22:41:29 +1000

    문자열 붙여서 출력하기 문제 해결

---
+++ src/main/java/me/programmer/level0/p181946/Main.java
@@ -0,0 +1,16 @@
+package me.programmer.level0.p181946;
+import java.util.Scanner;
+
+public class Main {
+}
+
+class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		String b = sc.next();
+
+		String answer = a + b;
+		System.out.println(answer);
+	}
+}
