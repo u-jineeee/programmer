989aafe4f29de814eb2e5471db59d6bed7dde172
ujineeee <yujin11006@naver.com>
2024-12-06 00:07:10 +1000

    공백으로 구분하기 1 문제 해결

---
+++ src/main/java/me/programmer/level0/p181869/Main.java
@@ -0,0 +1,9 @@
+package me.programmer.level0.p181869;
+
+public class Main {
+}
+class Solution {
+	public String[] solution(String my_string) {
+		return my_string.split(" ");
+	}
+}
---
+++ src/test/java/me/programmer/level0/p181869/Tests.java
@@ -0,0 +1,20 @@
+package me.programmer.level0.p181869;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Tests {
+	@Test
+	@DisplayName("example 1")
+	void test001() {
+		assertThat(new Solution().solution("i love you")).isEqualTo(new String[] {"i", "love", "you"});
+	}
+
+	@Test
+	@DisplayName("example 2")
+	void test002() {
+		assertThat(new Solution().solution("programmers")).isEqualTo(new String[] {"programmers"});
+	}
+}
