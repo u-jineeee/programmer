222feeb2ee65c8e9b53915c23a40b108998b75ce
ujineeee <yujin11006@naver.com>
2024-12-07 13:53:48 +1000

    A 강조하기 문제 해결

---
+++ src/main/java/me/programmer/level0/p181874/Main.java
@@ -0,0 +1,11 @@
+package me.programmer.level0.p181874;
+
+public class Main {
+}
+class Solution {
+	public String solution(String myString) {
+		myString = myString.toLowerCase();
+		myString = myString.replaceAll("a", "A");
+		return myString;
+	}
+}
