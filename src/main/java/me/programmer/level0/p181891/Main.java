6b48231f4b620e1f7c6bb570b9258f9d26ced11a
ujineeee <yujin11006@naver.com>
2024-12-12 22:31:13 +1000

    순서 바꾸기 문제 해결

---
+++ src/main/java/me/programmer/level0/p181891/Main.java
@@ -0,0 +1,17 @@
+package me.programmer.level0.p181891;
+
+public class Main {
+}
+class Solution {
+	public int[] solution(int[] num_list, int n) {
+		int[] answer = new int[num_list.length];
+		int cnt = 0;
+
+		for(int i = n; i < num_list.length; i++)
+			answer[cnt++] = num_list[i];
+		for(int i = 0; i < n; i++)
+			answer[cnt++] = num_list[i];
+
+		return answer;
+	}
+}
