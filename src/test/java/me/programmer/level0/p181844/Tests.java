0607248b3e5d567bf17f38d3f004686a5358e44e
ujineeee <yujin11006@naver.com>
2024-12-16 18:42:41 +1000

    배열의 원소 삭제하기 테스트 추가

--- src/test/java/me/programmer/level0/p181844/Tests.java
+++ src/test/java/me/programmer/level0/p181844/Tests.java
@@ -9,6 +9,12 @@ public class Tests {
 	@Test
 	@DisplayName("example 1")
 	void test001() {
-		assertThat(new Solution())
+		assertThat(new Solution().solution(new int[]{293, 1000, 395, 678, 94}, new int[]{94, 777, 104, 1000, 1, 12})).isEqualTo(new int[]{293, 395, 678});
+	}
+
+	@Test
+	@DisplayName("example 2")
+	void test002() {
+		assertThat(new Solution().solution(new int[]{110, 66, 439, 785, 1}, new int[]{377, 823, 119, 43})).isEqualTo(new int[]{110, 66, 439, 785, 1});
 	}
 }
