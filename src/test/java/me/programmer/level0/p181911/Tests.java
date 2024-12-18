2d600f98e08091dd31ce51fa9644af768ba0d90c
ujineeee <yujin11006@naver.com>
2024-12-18 23:15:37 +1000

    부분 문자열 이어 붙여 문자열 만들기 문제 해결

---
+++ src/test/java/me/programmer/level0/p181911/Tests.java
@@ -0,0 +1,14 @@
+package me.programmer.level0.p181911;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Tests {
+	@Test
+	@DisplayName("example 1")
+	void test001() {
+		assertThat(new Solution().solution(new String[]{"progressive", "hamburger", "hammer", "ahocorasick"}, new int[][]{{0, 4}, {1, 2}, {3, 5}, {7, 7}})).isEqualTo("programmers");
+	}
+}
