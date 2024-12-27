824db0a861aabf4954029ab46d78ab421d641755
ujineeee <yujin11006@naver.com>
2024-12-27 22:22:23 +1000

    원하는 문자열 찾기 문제 해결

---
+++ src/main/java/me/programmer/level0/p181878/Main.java
@@ -0,0 +1,11 @@
+package me.programmer.level0.p181878;
+
+public class Main {
+}
+class Solution {
+	public int solution(String myString, String pat) {
+		myString = myString.toLowerCase();
+		pat = pat.toLowerCase();
+		return myString.contains(pat) ? 1 : 0;
+	}
+}
