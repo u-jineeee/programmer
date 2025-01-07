f09c774146977523e8cc3b760e74bd88f98c9695
ujineeee <yujin11006@naver.com>
2025-01-07 19:22:13 +1000

    문자열 돌리기 문제 해결

---
+++ src/main/java/me/programmer/level0/p181945/Main.java
@@ -0,0 +1,16 @@
+package me.programmer.level0.p181945;
+import java.util.Scanner;
+
+public class Main {
+}
+
+class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+
+		for(int i = 0; i < a.length(); i++){
+			System.out.println(a.charAt(i));
+		}
+	}
+}
