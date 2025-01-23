21d35dcec9918ac607cd374dc0101bc36cd0fac8
ujineeee <yujin11006@naver.com>
2025-01-23 23:57:11 +1000

    1로 만들기 문제 해결

---
+++ src/main/java/me/programmer/level0/p181880/Main.java
@@ -0,0 +1,22 @@
+package me.programmer.level0.p181880;
+
+public class Main {
+}
+class Solution {
+	public int solution(int[] num_list) {
+		int answer = 0;
+		for(int num : num_list) {
+			int cnt = 0;
+			while(num != 1) {
+				if(num % 2 == 0) {
+					num /= 2;
+				} else {
+					num = (num - 1) / 2;
+				}
+				cnt++;
+			}
+			answer += cnt;
+		}
+		return answer;
+	}
+}
