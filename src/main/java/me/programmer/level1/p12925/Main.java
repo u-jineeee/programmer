ba5b3dbb8686da605e7b22ee0d4abad5c494bf11
ujineeee <yujin11006@naver.com>
2025-03-20 22:21:14 +1000

    문자열을 정수로 바꾸기 문제 해결

---
+++ src/main/java/me/programmer/level1/p12925/Main.java
@@ -0,0 +1,9 @@
+package me.programmer.level1.p12925;
+
+public class Main {
+}
+class Solution {
+	public int solution(String s) {
+		return Integer.parseInt(s);
+	}
+}
---
+++ src/test/java/me/programmer/level1/p12925/Tests.java
@@ -0,0 +1,14 @@
+package me.programmer.level1.p12925;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Tests {
+	@Test
+	@DisplayName("example 1")
+	void test001() {
+		assertThat(new Solution().solution("-1234")).isEqualTo(-1234);
+	}
+}
