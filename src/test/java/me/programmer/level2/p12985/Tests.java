dc7d8793dc442acf14b6cde1a292f60f213c1970
ujineeee <yujin11006@naver.com>
2025-04-18 20:48:19 +1000

    예상 대진표 문제 해결

---
+++ src/test/java/me/programmer/level2/p12985/Tests.java
@@ -0,0 +1,14 @@
+package me.programmer.level2.p12985;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Tests {
+	@Test
+	@DisplayName("example 1")
+	void test001() {
+		assertThat(new Solution().solution(8, 4, 7)).isEqualTo(3);
+	}
+}
