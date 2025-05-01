369ca7cb923506fae2c30d87eb15fecd18ada799
ujineeee <yujin11006@naver.com>
2025-05-01 23:51:19 +1000

    게임 맵 최단거리 문제 해결

---
+++ src/main/java/me/programmer/level2/p1844/Main.java
@@ -0,0 +1,40 @@
+package me.programmer.level2.p1844;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+}
+class Solution {
+	public int solution(int[][] maps) {
+		Queue<int[]> queue = new LinkedList<>();
+		boolean[][] visited = new boolean[maps.length][maps[0].length];
+		int[] dr = {-1, 1, 0, 0};
+		int[] dc = {0, 0, -1, 1};
+
+		queue.add(new int[]{0, 0, 1});
+		visited[0][0] = true;
+		while(!queue.isEmpty()) {
+			int[] position = queue.poll();
+			int r = position[0];
+			int c = position[1];
+			int dist = position[2];
+
+			if(r == maps.length - 1 && c == maps[0].length - 1)
+				return dist;
+
+			for(int i = 0; i < 4; i++) {
+				int nr = r + dr[i];
+				int nc = c + dc[i];
+
+				if(nr < 0 || nr >= maps.length || nc < 0 || nc >= maps[0].length
+				|| maps[nr][nc] == 0 || visited[nr][nc])
+					continue;
+
+				visited[nr][nc] = true;
+				queue.add(new int[]{nr, nc, dist+1});
+			}
+		}
+		return -1;
+	}
+}
---
+++ src/test/java/me/programmer/level2/p1844/Tests.java
@@ -0,0 +1,20 @@
+package me.programmer.level2.p1844;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Tests {
+	@Test
+	@DisplayName("example 1")
+	void test001() {
+		assertThat(new Solution().solution(new int[][]{{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}})).isEqualTo(11);
+	}
+
+	@Test
+	@DisplayName("example 2")
+	void test002() {
+		assertThat(new Solution().solution(new int[][]{{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,0},{0,0,0,0,1}})).isEqualTo(-1);
+	}
+}
