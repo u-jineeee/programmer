bad6a61ba94025a67e967da4b9c5730e3f8f0aaa
u-jineeee <yujin11006@naver.com>
2025-09-05 14:29:16 +0900

    가장 큰 수 문제 해결

--- src/main/java/me/programmer/level2/p42746/Main.java
+++ src/main/java/me/programmer/level2/p42746/Main.java
@@ -10,7 +10,6 @@ class Solution {
         for (int i = 0; i < numbers.length; i++) {
             strNumbers[i] = String.valueOf(numbers[i]);
         }
-
         Arrays.sort(strNumbers, (s1, s2) -> (s2 + s1).compareTo((s1 + s2)));
 
         if(strNumbers[0].equals("0"))
